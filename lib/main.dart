7ed39fe876f699fcce7ef93893acad4821840e7a
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-11-19 11:22:45 -0300

    Finalizing first part: build a infinit name list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:english_words/english_words.dart';
@@ -7,27 +8,51 @@ void main() => runApp(StartupNames());
 class StartupNames extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: SafeArea(
-          child: Center(
-            child: Text(wordPair.asPascalCase)
-          ),
-          top: true,
-          right: true,
-          bottom: true,
-          left: false,
-        )
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
 class RandomWordsState extends State<RandomWords> {
-  
+  final _suggestions = <WordPair>[]; 
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if(i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if(index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
 }
