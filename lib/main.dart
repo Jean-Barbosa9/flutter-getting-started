6484096c217a269af46c925842e09705c928e390
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-11-22 18:05:02 -0300

    Adding buttons types as examples

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,9 @@ class NetNinjasTutorial extends StatelessWidget {
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 40.0),
-          Image(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1574388548306-1e8e7aea8be4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')),
+          Image.network(
+              'https://images.unsplash.com/photo-1574388548306-1e8e7aea8be4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+          ),
           Container(
             child: Text(
               'Here you see an image from internet',
@@ -49,9 +49,7 @@ class NetNinjasTutorial extends StatelessWidget {
             color: Colors.grey[350],
           ),
           SizedBox(height: 25.0),
-          Image(
-              image: AssetImage('assets/imgs/shuriken-comming.jpeg')
-          ),
+          Image.asset('assets/imgs/shuriken-comming.jpeg'),
           Container(
             child: Text(
               'Here you see a local image',
@@ -63,6 +61,63 @@ class NetNinjasTutorial extends StatelessWidget {
             color: Colors.grey[350],
           ),
           SizedBox(height: 25.0),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(5.0),
+            color: Colors.amber[300],
+            child: Text(
+              'These are some button examples',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 10.0),
+          FlatButton(
+            onPressed: () {
+              print('Flat button tapped');
+            },
+            color: Colors.green,
+            splashColor: Colors.teal,
+            child: Text(
+              'Flat Button'.toUpperCase(),
+              style: TextStyle(
+                color: Color(0xffffffff),
+                fontWeight: FontWeight.bold,
+              )
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('Raised buttom clicked');
+            },
+            color: Colors.orange,
+            child: Text(
+              'Raised Button'.toUpperCase(),
+              style: TextStyle(
+                color: Color(0xffffffff)
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: () {
+              print('Raised button with icon tapped');
+            },
+            shape: RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(20.0),
+            ),
+            icon: Icon(Icons.turned_in_not, color: Colors.red,),
+            label: Text('Raised button with icon'.toUpperCase()),
+          ),
+          IconButton(
+            onPressed: () {
+              print('Icon button tapped');
+            },
+            alignment: Alignment.centerLeft,
+            icon: Icon(
+              Icons.send,
+              color: Color.fromRGBO(150,100,200, 0.5),
+              size: 50.0,
+            ),
+          ),
         ],
       )),
       floatingActionButton: FloatingActionButton(
