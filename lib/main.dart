ce9bfe1ab00f6ac3496ba402288f6d74f8da16b5
Jean Barbosa <jeanbarbosa9@gmail.com>
2019-11-22 16:37:28 -0300

    Rendering images in a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 // Code base on Net Ninja YouTube Tutorial
 
-void main() =>
-    runApp(
+void main() => runApp(
       MaterialApp(
         title: 'Net Ninja Tutorial',
         home: NetNinjasTutorial(),
@@ -20,17 +21,50 @@ class NetNinjasTutorial extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-        'Hello ninjas',
-        style: TextStyle(
-          fontFamily: 'IndieFlower',
-          fontSize: 25.0,
-          color: Colors.red[600],
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          )
-        )
-      ),
+          child: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: <Widget>[
+          Text('Hello ninjas',
+            style: TextStyle(
+              fontFamily: 'IndieFlower',
+              fontSize: 25.0,
+              color: Colors.red[600],
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 40.0),
+          Image(
+              image: NetworkImage(
+                  'https://images.unsplash.com/photo-1574388548306-1e8e7aea8be4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80')),
+          Container(
+            child: Text(
+              'Here you see an image from internet',
+              style: TextStyle(fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            width: double.infinity,
+            padding: EdgeInsets.all(5.0),
+            color: Colors.grey[350],
+          ),
+          SizedBox(height: 25.0),
+          Image(
+              image: AssetImage('assets/imgs/shuriken-comming.jpeg')
+          ),
+          Container(
+            child: Text(
+              'Here you see a local image',
+              style: TextStyle(fontSize: 16.0),
+              textAlign: TextAlign.center,
+            ),
+            width: double.infinity,
+            padding: EdgeInsets.all(5.0),
+            color: Colors.grey[350],
+          ),
+          SizedBox(height: 25.0),
+        ],
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('click'),
